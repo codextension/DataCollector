b931ca22bba62630fe6af4c86954a42c5f3d6271
Elie Khoury <elie@el-khoury.net>
2018-05-18 21:06:32 +0200

    alt solution

--- app/src/main/java/com/nuance/labs/datacollector/MainActivity.java
+++ app/src/main/java/com/nuance/labs/datacollector/MainActivity.java
@@ -1,9 +1,25 @@
 package com.nuance.labs.datacollector;
 
+import android.content.Context;
 import android.content.Intent;
+import android.location.Location;
+import android.location.LocationManager;
+import android.os.Environment;
+import android.os.Handler;
+import android.os.IBinder;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.telephony.PhoneStateListener;
+import android.telephony.SignalStrength;
+import android.telephony.TelephonyManager;
+import android.util.Log;
 import android.view.WindowManager;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.PrintStream;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -14,7 +30,122 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
-        Intent serviceIntent= new Intent(this,BackgroundService.class);
-        startService(serviceIntent);
+
+        Handler mHandler = new Handler(getMainLooper());
+        mHandler.post(new Runnable() {
+            @Override
+            public void run() {
+                initializeLocationManager();
+                try {
+                    mLocationManager.requestLocationUpdates(
+                            LocationManager.GPS_PROVIDER,
+                            LOCATION_INTERVAL,
+                            LOCATION_DISTANCE,
+                            mLocationListeners[0]
+                    );
+                } catch (java.lang.SecurityException ex) {
+                    Log.i(TAG, "fail to request location update, ignore", ex);
+                } catch (IllegalArgumentException ex) {
+                    Log.d(TAG, "network provider does not exist, " + ex.getMessage());
+                }
+            }
+        });
+    }
+
+    private static final String TAG = "BackgroundService";
+    private LocationManager mLocationManager = null;
+    private static final int LOCATION_INTERVAL = 1000;
+    private static final float LOCATION_DISTANCE = 50f;
+
+    private class MyPhoneStateListener extends PhoneStateListener {
+        private final Location location;
+        private final int networkType;
+        protected SignalStrength signalStrength;
+
+        public MyPhoneStateListener(Location location, int networkType){
+            this.location = location;
+            this.networkType = networkType;
+        }
+
+        @Override
+        public void onSignalStrengthsChanged(SignalStrength signalStrength) {
+            super.onSignalStrengthsChanged(signalStrength);
+            this.signalStrength = signalStrength;
+            Log.i(TAG,"Signal quality: " + signalStrength.getLevel() + " at location " + location.toString());
+            try {
+                File storageFile = getStorageFile();
+                FileOutputStream fileOutputStream = new FileOutputStream(storageFile,true);
+                PrintStream printStream = new PrintStream(fileOutputStream);
+                printStream.print(location.getLongitude()+";"+location.getLatitude()+";"+location.getAltitude()+";"+location.getTime()+";"+signalStrength.getLevel() + ";" +NetworkType.fromInt(networkType).name() +"\n");
+                fileOutputStream.close();
+            } catch (IOException e) {
+                Log.e(TAG, e.getMessage());
+            }
+
+        }
+    }
+
+    public File getStorageFile() throws IOException {
+        // Get the directory for the user's public pictures directory.
+        File file = new File(Environment.getExternalStoragePublicDirectory(
+                Environment.DIRECTORY_DOWNLOADS), "data.cvs");
+        if (!file.exists()) {
+            file.createNewFile();
+            FileOutputStream fileOutputStream = new FileOutputStream(file,true);
+            PrintStream printStream = new PrintStream(fileOutputStream);
+            printStream.print("longitude;latitude;altitude;timestamp;signal_strength;network_type\n");
+            fileOutputStream.close();
+
+        }
+        return file;
+    }
+
+    private class LocationListener implements android.location.LocationListener {
+        Location mLastLocation;
+
+        public LocationListener(String provider) {
+            Log.e(TAG, "LocationListener " + provider);
+            mLastLocation = new Location(provider);
+        }
+
+        @Override
+        public void onLocationChanged(Location location) {
+            Log.e(TAG, "onLocationChanged: " + location);
+            mLastLocation.set(location);
+            TelephonyManager mTelephonyManager= (TelephonyManager) getSystemService(Context.TELEPHONY_SERVICE);
+            Log.i(TAG,NetworkType.fromInt(mTelephonyManager.getNetworkType()).name());
+            MyPhoneStateListener phoneStateListener = new MyPhoneStateListener(location,mTelephonyManager.getNetworkType());
+            mTelephonyManager.listen(phoneStateListener, PhoneStateListener.LISTEN_SIGNAL_STRENGTHS);
+            mTelephonyManager.listen(phoneStateListener, PhoneStateListener.LISTEN_NONE);
+        }
+
+        @Override
+        public void onProviderDisabled(String provider) {
+            Log.e(TAG, "onProviderDisabled: " + provider);
+        }
+
+        @Override
+        public void onProviderEnabled(String provider) {
+            Log.e(TAG, "onProviderEnabled: " + provider);
+        }
+
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+            Log.e(TAG, "onStatusChanged: " + provider);
+        }
+    }
+
+
+    LocationListener[] mLocationListeners = new LocationListener[]{
+            new LocationListener(LocationManager.GPS_PROVIDER),
+            new LocationListener(LocationManager.NETWORK_PROVIDER),
+            new LocationListener(LocationManager.PASSIVE_PROVIDER)
+    };
+
+    private void initializeLocationManager() {
+        Log.e(TAG, "initializeLocationManager - LOCATION_INTERVAL: "+ LOCATION_INTERVAL + " LOCATION_DISTANCE: " + LOCATION_DISTANCE);
+        if (mLocationManager == null) {
+            mLocationManager = (LocationManager) getApplicationContext().getSystemService(Context.LOCATION_SERVICE);
+        }
     }
 }
