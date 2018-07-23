3808d8edfacaa7454509fe5db13acfeae8212616
Elie Khoury <elie@el-khoury.net>
2018-07-23 13:36:44 +0200

    updated the code

--- app/src/main/java/com/nuance/labs/datacollector/BackgroundService.java
+++ app/src/main/java/com/nuance/labs/datacollector/BackgroundService.java
@@ -42,60 +42,89 @@ import java.util.List;
  */
 public class BackgroundService extends Service {
     private static final String TAG = "BackgroundService";
-    private static final int LOCATION_INTERVAL = 1000;//1000;
-    private static final float LOCATION_DISTANCE = 50f;//50f;
+    private static final int LOCATION_INTERVAL = 0;//1000;
+    private static final float LOCATION_DISTANCE = 0f;//50f;
     private LocationManager mLocationManager = null;
-
-
-    private class MyPhoneStateListener extends PhoneStateListener {
-        private final Location location;
-        private final int networkType;
-        protected SignalStrength signalStrength;
-
-        public MyPhoneStateListener(Location location, int networkType){
-            this.location = location;
-            this.networkType = networkType;
-        }
-
-        @Override
-        public void onSignalStrengthsChanged(SignalStrength signalStrength) {
-            super.onSignalStrengthsChanged(signalStrength);
-            this.signalStrength = signalStrength;
-            Log.i(TAG,NetworkType.fromInt(networkType).name() + " - Signal quality: " + signalStrength.getLevel() + " at location " + location.toString());
-            try {
-                File storageFile = getStorageFile();
-                FileOutputStream fileOutputStream = new FileOutputStream(storageFile,true);
-                PrintStream printStream = new PrintStream(fileOutputStream);
-                printStream.print(location.getLongitude()+";"+location.getLatitude()+";"+location.getAltitude()+";"+location.getTime()+";"+signalStrength.getLevel() + ";" +NetworkType.fromInt(networkType).name() +"\n");
-                fileOutputStream.close();
-            } catch (IOException e) {
-                Log.e(TAG, e.getMessage());
-            }
-
-        }
-    }
+    private TelephonyManager mTelephonyManager = null;
+    private MyPhoneStateListener phoneStateListener;
 
     public File getStorageFile() throws IOException {
         // Get the directory for the user's public pictures directory.
-        DateFormat dateFormat = new SimpleDateFormat("yyyy_mm_dd");
+        DateFormat dateFormat = new SimpleDateFormat("yyyy_MM_dd");
 
         //to convert Date to String, use format method of SimpleDateFormat class.
         String strDate = dateFormat.format(new Date());
         File file = new File(Environment.getExternalStoragePublicDirectory(
-                Environment.DIRECTORY_DCIM), strDate + ".cvs");
+                Environment.DIRECTORY_DOWNLOADS), strDate + ".cvs");
         if (!file.exists()) {
             file.createNewFile();
             FileOutputStream fileOutputStream = new FileOutputStream(file,true);
             PrintStream printStream = new PrintStream(fileOutputStream);
-            printStream.print("longitude;latitude;altitude;timestamp;signal_strength;network_type\n");
+            //printStream.print("longitude;latitude;altitude;timestamp;signal_strength;network_type\n");
             fileOutputStream.close();
 
         }
         return file;
     }
 
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        initializeLocationManager();
+        try {
+            mLocationManager.requestLocationUpdates(
+                    LocationManager.GPS_PROVIDER,
+                    LOCATION_INTERVAL,
+                    LOCATION_DISTANCE,
+                    mLocationListeners[0]
+            );
+
+            phoneStateListener = new MyPhoneStateListener();
+            mTelephonyManager = (TelephonyManager) getSystemService(Context.TELEPHONY_SERVICE);
+            mTelephonyManager.listen(phoneStateListener, PhoneStateListener.LISTEN_SIGNAL_STRENGTHS);
+        } catch (java.lang.SecurityException ex) {
+            Log.e(TAG, "fail to request location update, ignore", ex);
+        } catch (IllegalArgumentException ex) {
+            Log.e(TAG, "network provider does not exist, " + ex.getMessage());
+        }
+
+        return Service.START_NOT_STICKY;
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mLocationManager.removeUpdates(mLocationListeners[0]);
+        mTelephonyManager.listen(phoneStateListener, PhoneStateListener.LISTEN_NONE);
+    }
+
+
+    private LocationListener[] mLocationListeners = new LocationListener[]{
+            new LocationListener(LocationManager.GPS_PROVIDER),
+            new LocationListener(LocationManager.NETWORK_PROVIDER),
+            new LocationListener(LocationManager.PASSIVE_PROVIDER)
+    };
+
+    @Override
+    public IBinder onBind(Intent arg0) {
+        return null;
+    }
+
+    private class MyPhoneStateListener extends PhoneStateListener {
+        private SignalStrength signalStrength;
+
+        public SignalStrength getSignalStrength() {
+            return signalStrength;
+        }
+
+        @Override
+        public void onSignalStrengthsChanged(SignalStrength signalStrength) {
+            super.onSignalStrengthsChanged(signalStrength);
+            this.signalStrength = signalStrength;
+        }
+    }
+
     private class LocationListener implements android.location.LocationListener {
-        Location mLastLocation;
+        private Location mLastLocation;
 
         public LocationListener(String provider) {
             Log.i(TAG, "LocationListener " + provider);
@@ -104,11 +133,20 @@ public class BackgroundService extends Service {
 
         @Override
         public void onLocationChanged(Location location) {
-            mLastLocation.set(location);
-            TelephonyManager mTelephonyManager= (TelephonyManager) getSystemService(Context.TELEPHONY_SERVICE);
-            MyPhoneStateListener phoneStateListener = new MyPhoneStateListener(location,mTelephonyManager.getNetworkType());
-            mTelephonyManager.listen(phoneStateListener, PhoneStateListener.LISTEN_SIGNAL_STRENGTHS);
-            mTelephonyManager.listen(phoneStateListener, PhoneStateListener.LISTEN_NONE);
+
+            if (mLastLocation.getLatitude() != location.getLatitude() || mLastLocation.getLongitude() != location.getLongitude()) {
+                mLastLocation.set(location);
+
+                try {
+                    File storageFile = getStorageFile();
+                    FileOutputStream fileOutputStream = new FileOutputStream(storageFile, true);
+                    PrintStream printStream = new PrintStream(fileOutputStream);
+                    printStream.print(location.getLongitude() + ";" + location.getLatitude() + ";" + location.getAltitude() + ";" + location.getTime() + ";" + phoneStateListener.getSignalStrength().getLevel() + ";" + NetworkType.fromInt(mTelephonyManager.getNetworkType()).name() + "\n");
+                    fileOutputStream.close();
+                } catch (IOException e) {
+                    Log.e(TAG, e.getMessage());
+                }
+            }
         }
 
         @Override
@@ -125,43 +163,6 @@ public class BackgroundService extends Service {
         public void onStatusChanged(String provider, int status, Bundle extras) {
             Log.i(TAG, "onStatusChanged: " + provider);
         }
-    }
-
-
-    private LocationListener[] mLocationListeners = new LocationListener[]{
-            new LocationListener(LocationManager.GPS_PROVIDER),
-            new LocationListener(LocationManager.NETWORK_PROVIDER),
-            new LocationListener(LocationManager.PASSIVE_PROVIDER)
-    };
-
-    @Override
-    public IBinder onBind(Intent arg0) {
-        return null;
-    }
-
-    @Override
-    public int onStartCommand(Intent intent, int flags, int startId) {
-        initializeLocationManager();
-        try {
-            mLocationManager.requestLocationUpdates(
-                    LocationManager.GPS_PROVIDER,
-                    LOCATION_INTERVAL,
-                    LOCATION_DISTANCE,
-                    mLocationListeners[0]
-            );
-        } catch (java.lang.SecurityException ex) {
-            Log.e(TAG, "fail to request location update, ignore", ex);
-        } catch (IllegalArgumentException ex) {
-            Log.e(TAG, "network provider does not exist, " + ex.getMessage());
-        }
-
-        return Service.START_NOT_STICKY;
-    }
-
-    @Override
-    public void onDestroy() {
-        super.onDestroy();
-        mLocationManager.removeUpdates(mLocationListeners[0]);
     }
 
     private void initializeLocationManager() {
